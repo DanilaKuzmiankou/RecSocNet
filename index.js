6190813e877ab72722c7466e062708631ce258a2
St4ffer <stmail468@gmail.com>
2022-02-20 23:45:13 +0300

    commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('client/build'));
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
     // app.get('/profile', (req, res) => {
