3e310b20b689aee3838cb64ec06ead4a09cab30c
St4ffer <stmail468@gmail.com>
2022-02-20 23:42:14 +0300

    commit

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ if(process.env.NODE_ENV === "production"){
     app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
-    app.get('/profile', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+    // app.get('/profile', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // })
 }
 
 app.use(cors({origin: true}))
