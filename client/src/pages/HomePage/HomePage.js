f9492d9b80c5249277dfa3b15a53230f6578a2da
St4ffer <stmail468@gmail.com>
2022-02-20 21:11:28 +0300

    commit

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,14 +1,22 @@
 import {Button, Container} from "react-bootstrap";
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import {useAuth0} from "@auth0/auth0-react";
 
 export const HomePage = () => {
     const navigate = useNavigate()
+    const {user, isAuthenticated} = useAuth0()
 
     const routeChange = () => {
         let path = `/profile`;
-        console.log('lol')
         navigate(path);
+    }
+    const getInfo = () => {
+        console.log("checking...")
+        if(isAuthenticated)
+        {
+            console.log(user)
+        }
     }
 
     return (
@@ -17,6 +25,7 @@ export const HomePage = () => {
             <Container fluid >
                 <div>
                     <h1 > Welcome to the Rec Soc Net Site Home Page! </h1>
+                    <Button variant="danger"  onClick={routeChange}>Get Started!</Button>
                     <Button variant="danger"  onClick={routeChange}>Get Started!</Button>
                 </div>
             </Container>
