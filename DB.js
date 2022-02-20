a936d26415df17a6b040f50a4eb54489cf307db2
St4ffer <stmail468@gmail.com>
2022-02-20 19:52:55 +0300

    commit

--- DB.js
+++ DB.js
@@ -1,12 +1,26 @@
 const {Sequelize} = require('sequelize')
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env. DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        logging:false
-    }
-)
+
+if(process.env.NODE_ENV === "production"){
+    module.exports = new Sequelize(process.env.DATABASE_URL, {
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            }
+        }
+    );
+}
+else {
+    module.exports = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            dialect: 'postgres',
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            logging: false
+        }
+    )
+}
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if(process.env.NODE_ENV === "production"){
     app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
-    app.get('/userPage', (req, res) => {
+    app.get('/profile', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
