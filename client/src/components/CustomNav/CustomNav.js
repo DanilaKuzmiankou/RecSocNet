53eed9c7daadc6d93c498bdc56a9015d2cda3de2
St4ffer <stmail468@gmail.com>
2022-02-21 21:09:29 +0300

    commit

---
+++ client/src/components/CustomNav/CustomNav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import "../../App.css"
+import {OverlayTrigger, Tooltip} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
+
+export const CustomNav = () => {
+
+    const navigate = useNavigate()
+    const routeChange = () => {
+        let path = `/profile`;
+        navigate(path);
+    }
+
+return (
+    <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+        <div className="d-flex flex-grow-1 navbar_home_button_container">
+            <OverlayTrigger placement="bottom" delay={{show: 250, hide: 400}}
+                            overlay={<Tooltip id="tooltip-disabled">To Home!</Tooltip>}>
+                <a href="/" className=" justify-content-start navbar_home_button">Home</a>
+            </OverlayTrigger>
+        </div>
+        <div className="navbar-collapse collapse w-100 navbar_auth_button" id="collapsingNavbar3">
+            <ul className="nav navbar-nav ml-auto w-100 justify-content-end">
+                <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-disabled">To Profile!</Tooltip>}>
+                    <a className="navbar_users_data_button" onClick={routeChange}>Profile</a>
+                </OverlayTrigger>
+            </ul>
+        </div>
+    </nav>
+)
+
+};
+
+export default CustomNav;
