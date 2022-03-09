1fe97cc2a50e61bdecfd657ee8ccbd6143f75711
St4ffer <stmail468@gmail.com>
2022-03-09 19:23:52 +0300

    Adaptive bootstrap navbar compatability improved

--- client/src/components/CustomNav/CustomNav.js
+++ client/src/components/CustomNav/CustomNav.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import "../../App.css"
-import {Button, Container, Form, FormControl, Nav, Navbar, NavDropdown, OverlayTrigger, Tooltip} from "react-bootstrap";
+import {
+    Button, Col,
+    Container,
+    Form,
+    FormControl,
+    Nav,
+    Navbar,
+    NavDropdown,
+    OverlayTrigger,
+    Row,
+    Tooltip
+} from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
 import {AuthButton} from "../index.components"
 
@@ -12,41 +23,60 @@ export const CustomNav = () => {
         navigate(path);
     }
 
-return (
+    return (
 
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container fluid>
-            <OverlayTrigger placement="bottom" delay={{show: 250, hide: 400}}
-                            overlay={<Tooltip id="tooltip-disabled">To Home!</Tooltip>}>
-                <a href="/" className=" justify-content-start navbar_home_button">Home</a>
-            </OverlayTrigger>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="navbarCollapse">
-                <Nav
-                    className="me-auto my-2 my-lg-0"
-                    style={{ maxHeight: '100px' }}
-                    navbarScroll
-                >
-                </Nav>
-                <OverlayTrigger placement="bottom" overlay={<Tooltip id="tooltip-disabled">To Profile!</Tooltip>}>
-                    <a className="navbar_users_data_button" onClick={routeChange}>Profile</a>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container fluid>
+                <OverlayTrigger placement="bottom" delay={{show: 250, hide: 400}}
+                                overlay={<Tooltip id="tooltip-disabled">To Home!</Tooltip>}>
+                    <a href="/" className=" justify-content-start navbar_home_button">Home</a>
                 </OverlayTrigger>
-                <Form className="d-flex">
-                    <FormControl
-                        type="search"
-                        placeholder="Search reviews"
-                        className="me-2"
-                        aria-label="Search"
-                    />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
 
 
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav
 
-)
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px', gap: "1rem"}}>
+
+                    </Nav>
+
+                    <div>
+                        <Container >
+                            <Row  >
+                                <Col lg={"auto"} md={12} className="nav_elements_margin">
+                                <OverlayTrigger placement="bottom"
+                                                overlay={<Tooltip id="tooltip-disabled">To Profile!</Tooltip>}>
+                                    <a className="navbar_users_data_button" onClick={routeChange}>Profile</a>
+                                </OverlayTrigger>
+                                </Col>
+
+                                <Col lg={"auto"} md={12} className="nav_elements_margin">
+                                    <Form className="d-flex">
+                                        <FormControl
+                                            type="search"
+                                            placeholder="Search reviews"
+                                            className="me-2"
+                                            aria-label="Search"
+                                        />
+                                        <Button variant="outline-success">Search</Button>
+                                    </Form>
+                                </Col>
+
+                                <Col lg={"auto"} md={12} className="nav_elements_margin" >
+                                    <AuthButton/>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+
+    )
 
 };
 
