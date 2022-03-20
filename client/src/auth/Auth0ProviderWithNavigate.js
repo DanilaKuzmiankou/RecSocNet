4aea9a76bb27b08650b49e83706a8676d939a8ba
St4ffer <stmail468@gmail.com>
2022-03-20 22:45:27 +0300

    register hook created and brought to the top level

--- client/src/auth/Auth0ProviderWithNavigate.js
+++ client/src/auth/Auth0ProviderWithNavigate.js
@@ -1,30 +1,27 @@
-import {Auth0Provider} from '@auth0/auth0-react'
-import {useNavigate} from "react-router-dom";
+import { Auth0Provider } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
+const Auth0ProviderWithNavigate = ({ children }) => {
+  const navigate = useNavigate();
 
-const Auth0ProviderWithNavigate = ({children}: React.ReactNode) => {
-    const navigate = useNavigate()
+  const onRedirectCallback = (appState) => {
+    // the path to redirect is specified in the `returnTo` property
+    // by default the user is returned to the current page
+    navigate(appState || window.location.pathname);
+  };
 
+  return (
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
 
-    const onRedirectCallback = (appState: { returnTo?: string }) => {
-        //the path to redirect is specified in the `returnTo` property
-        // by default the user is returned to the current page
-        navigate(appState?.returnTo || window.location.pathname)
-    }
-
-
-    return (
-        <Auth0Provider
-            domain={process.env.REACT_APP_AUTH0_DOMAIN}
-            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-            audience={process.env.REACT_APP_AUTH0_AUDIENCE}
-            redirectUri={window.location.origin}
-            onRedirectCallback={onRedirectCallback}
-        >
-            {children}
-        </Auth0Provider>
-    )
-}
-
-
-export default Auth0ProviderWithNavigate
+export default Auth0ProviderWithNavigate;
